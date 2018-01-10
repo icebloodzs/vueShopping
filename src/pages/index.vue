9abf849095fcedf8f60ab7f25c15d4dadcb4bde6
xiaojie.zhu <andy771480011@gmail.com>
2018-01-10 10:10:54 +0800

    first modify front code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,41 +1,43 @@
 <template>
-<!--首页-->
-    <div class="wrapper">
-        <!-- 轮播 -->
-        <app-banner :listImg="listImg" ></app-banner>
-        <div class="circle index"  @click="routerClickhome">首页</div>
-        <div class="circle personal"  @click="routerClickcenter">个人<br>中心</div>
-        <div class="classify">
-            <div class="classify-list list1" v-for="(item,index) in classify"><img :src="item.icon" >{{item.name}}</div>
-        </div>
-        <div class="more">
-            <span>大牌抢购</span>
-            <span  @click="routerClick">更多<i class="iconfont">&#xe62d;</i></span>
-        </div>
-        <div class="left-icon"></div>
-        <div class="content-item" v-for="(item,index) in goodsList">
-            <router-link :to="{path:'/dist/details'}" tag="img" class="content-img" :src="item.thumbnail">立即抢购</router-link>
-            <div class="content-middle">
-                <span><strong>{{item.name}}</strong></span>
-                <!-- <span> <strong>五折优惠</strong></span> -->
-                <div class="price">
-                    <span>{{item.original_price}}</span>
-                    <span>元</span>
-                    <span>{{item.price_spike}}</span>
-                </div>
-                <div class="remain">
-                   <mu-linear-progress :max="200" class="progress" mode="determinate" color="#1e7fea"  :value="item.surplus"/>
-                   <span>剩余{{item.surplus}}份</span>
-                </div>
-            </div>
-             <router-link :to="{path:'/dist/details',query:{id:item.id,lng:116.30387397,lat:39.91481908}}" tag="div" class="content-right">立即抢购</router-link>
-        </div>
+  <!--首页-->
+  <div class="wrapper">
+    <!-- 轮播 -->
+    <app-banner :listImg="listImg"></app-banner>
+    <div class="circle index" @click="routerClickhome">首页</div>
+    <div class="circle personal" @click="routerClickcenter">个人<br>中心</div>
+    <div class="classify">
+      <div class="classify-list list1" v-for="(item,index) in classify"><img :src="item.icon">{{item.name}}</div>
     </div>
+    <div class="more">
+      <span>大牌抢购</span>
+      <span @click="routerClick">更多<i class="iconfont">&#xe62d;</i></span>
+    </div>
+    <div class="left-icon"></div>
+    <div class="content-item" v-for="(item,index) in goodsList">
+      <router-link :to="{path:'/dist/details'}" tag="img" class="content-img" :src="item.thumbnail">立即抢购</router-link>
+      <div class="content-middle">
+        <span><strong>{{item.name}}</strong></span>
+        <!-- <span> <strong>五折优惠</strong></span> -->
+        <div class="price">
+          <span>{{item.original_price}}</span>
+          <span>元</span>
+          <span>{{item.price_spike}}</span>
+        </div>
+        <div class="remain">
+          <mu-linear-progress :max="200" class="progress" mode="determinate" color="#1e7fea" :value="item.surplus"/>
+          <span>剩余{{item.surplus}}份</span>
+        </div>
+      </div>
+      <router-link :to="{path:'/dist/details',query:{id:item.id,lng:116.30387397,lat:39.91481908}}" tag="div"
+                   class="content-right">立即抢购
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-import $ from "jquery";
-import Banner from "../components/Banner.vue";
+  import $ from "jquery";
+  import Banner from "../components/Banner.vue";
 
 
 import axios from "axios";
@@ -67,7 +69,7 @@ export default {
     routerClickcenter() {
       this.$router.push("/dist/mycenter");
     },
-  
+
     routerClickhome() {
       this.$router.go(0);
     },
@@ -102,168 +104,170 @@ export default {
       console.log(that.goodsList)
       })
     }
-    
+
   }
 };
 </script>
 
 <style scoped lang="scss">
-@import "../style/mixin";
+  @import "../style/mixin";
 
-.wrapper {
-  position: relative;
-  background-color: #fbfcfe;
-  height: 100vh;
-  .circle {
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: rem(80);
-    height: rem(82);
-    border-radius: rem(40);
-    background-color: #232323;
-    color: #fff;
-    font-size: rem(22);
-     opacity: .8;
-    z-index: 1;
-  }
-  .index {
-    line-height: rem(80);
-    top: rem(55);
-    left: rem(655);
-  }
-  .personal {
-    top: rem(150);
-    left: rem(655);
-  }
-
-  .classify {
-    display: flex;
-    justify-content: space-around;
-    background-color: #fff;
-    height: rem(170);
-
-    .classify-list {
-      // width: 25%;
-      height: rem(170);
+  .wrapper {
+    position: relative;
+    background-color: #fbfcfe;
+    height: 100vh;
+    .circle {
+      position: absolute;
       display: flex;
-      flex-direction: column;
+      justify-content: center;
       align-items: center;
-      font-size: rem(27);
-      img {
-        margin-top: rem(35);
-        margin-bottom: rem(20);
-        width: rem(60);
-        height: rem(50);
-      }
+      width: rem(80);
+      height: rem(82);
+      border-radius: rem(40);
+      background-color: #232323;
+      color: #fff;
+      font-size: rem(22);
+      opacity: .8;
+      z-index: 1;
     }
-  }
-  .more {
-    display: flex;
-    justify-content: space-between;
-    height: rem(90);
-    align-items: center;
-    background-color: #fff;
-    margin-top: rem(20);
-    border-bottom: 1px solid #e9e9e9;
-    span:nth-child(1) {
-      font-size: rem(33);
-      font-weight: 600;
-      margin-left: rem(37);
+    .index {
+      line-height: rem(80);
+      top: rem(55);
+      left: rem(655);
     }
-    span:nth-child(2) {
-      font-size: rem(30);
-      color: #17bafb;
-      margin-right: rem(35);
-        .iconfont{
-          font-family:"iconfont"; font-style:normal;margin-left: rem(13);
-          }
+    .personal {
+      top: rem(150);
+      left: rem(655);
     }
-  }
-  .left-icon{
-    width: rem(15);
-    height: rem(45);
-    background-color: #1e7fea;
-    position: absolute;
-    border-radius: 0 rem(6) rem(6) 0;
-    top: rem(504);
-    left: 0;
-  }
-  .content-item {
-    height: rem(212);
-    background-color: #fff;
-    display: flex;
-    flex-direction: row;
-    .content-img {
-      width: rem(212);
-    }
-    .content-middle {
+
+    .classify {
       display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      margin-top: rem(28);
-      span:nth-child(1) {
-        width:rem(300);
-        font-size: rem(30);
-        line-height: rem(30);
+      justify-content: space-around;
+      background-color: #fff;
+      height: rem(170);
+
+      .classify-list {
+        // width: 25%;
+        height: rem(170);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: rem(27);
+        img {
+          margin-top: rem(35);
+          margin-bottom: rem(20);
+          width: rem(60);
+          height: rem(50);
+        }
       }
-      .price {
-        span {
+    }
+    .more {
+      display: flex;
+      justify-content: space-between;
+      height: rem(90);
+      align-items: center;
+      background-color: #fff;
+      margin-top: rem(20);
+      border-bottom: 1px solid #e9e9e9;
+      span:nth-child(1) {
+        font-size: rem(33);
+        font-weight: 600;
+        margin-left: rem(37);
+      }
+      span:nth-child(2) {
+        font-size: rem(30);
+        color: #17bafb;
+        margin-right: rem(35);
+        .iconfont {
+          font-family: "iconfont";
+          font-style: normal;
+          margin-left: rem(13);
+        }
+      }
+    }
+    .left-icon {
+      width: rem(15);
+      height: rem(45);
+      background-color: #1e7fea;
+      position: absolute;
+      border-radius: 0 rem(6) rem(6) 0;
+      top: rem(504);
+      left: 0;
+    }
+    .content-item {
+      height: rem(212);
+      background-color: #fff;
+      display: flex;
+      flex-direction: row;
+      .content-img {
+        width: rem(212);
+      }
+      .content-middle {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin-top: rem(28);
+        span:nth-child(1) {
+          width: rem(300);
+          font-size: rem(30);
+          line-height: rem(30);
+        }
+        .price {
+          span {
+            font-size: rem(25);
+            color: #d2d2d2;
+            text-decoration: line-through;
+            line-height: 1.5;
+          }
+          span:nth-child(1) {
+            color: #1e80eb;
+            font-size: rem(40);
+
+            text-decoration: none;
+          }
+          span:nth-child(2) {
+            font-size: rem(25);
+            color: #1e7fea;
+            text-decoration: none;
+          }
+        }
+        .remain {
+          display: flex;
+          flex-direction: row;
           font-size: rem(25);
           color: #d2d2d2;
-          text-decoration: line-through;
-          line-height: 1.5;
-        }
-        span:nth-child(1) {
-          color: #1e80eb;
-          font-size: rem(40);
-
-          text-decoration: none;
-        }
-        span:nth-child(2) {
-          font-size: rem(25);
-          color: #1e7fea;
-          text-decoration: none;
+          height: rem(18);
+          line-height: rem(18);
+          justify-content: space-between;
+          flex-wrap: nowrap;
+          .progress {
+            width: rem(200);
+            margin-right: rem(6);
+            height: rem(14);
+            border-radius: rem(7);
+            background-color: #e4e4e4;
+          }
+          span {
+            display: block;
+            color: #666;
+            height: rem(28);
+            font-size: rem(28);
+            line-height: rem(28);
+            margin-top: rem(-8);
+          }
         }
       }
-      .remain {
-        display: flex;
-        flex-direction: row;
+      .content-right {
+        background-color: #ff9c00;
+        margin: auto;
+        width: rem(150);
+        height: rem(66);
         font-size: rem(25);
-        color: #d2d2d2;
-        height: rem(18);
-        line-height: rem(18);
-        justify-content: space-between;
-        flex-wrap: nowrap;
-        .progress {
-          width: rem(200);
-          margin-right: rem(6);
-          height: rem(14);
-          border-radius: rem(7);
-          background-color: #e4e4e4;
-        }
-        span {
-          display: block;
-          color: #666;
-          height: rem(28);
-          font-size: rem(28);
-          line-height: rem(28);
-          margin-top: rem(-8);
-        }
+        color: #fff;
+        text-align: center;
+        line-height: rem(66);
+        border-radius: rem(5);
       }
-    }
-    .content-right {
-      background-color: #ff9c00;
-      margin: auto;
-      width: rem(150);
-      height: rem(66);
-      font-size: rem(25);
-      color: #fff;
-      text-align: center;
-      line-height: rem(66);
-      border-radius: rem(5);
     }
   }
-}
 </style>
