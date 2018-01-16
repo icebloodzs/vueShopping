cb617d3701b536b9493d8d3fa24c64e7f75c5cd7
icebloodzs <905064863@qq.com>
2018-01-16 13:38:11 +0800

    recover

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,46 @@
 <template>
-<!--首页-->
-    <div class="wrapper">
-        <!-- 轮播 -->
-        <app-banner :listImg="listImg" ></app-banner>
-        <div class="circle index"  @click="routerClickhome">首页</div>
-        <div class="circle personal"  @click="routerClickcenter">个人<br>中心</div>
-        <div class="classify">
-            <div class="classify-list list1" v-for="item in classify" @click="clickClassify(item.id)"><img :src="item.icon" >{{item.name}}</div>
-        </div>
-        <div class="more">
-            <span>大牌抢购</span>
-            <span  @click="routerClick">更多<i class="iconfont">&#xe62d;</i></span>
-        </div>
-        <div class="left-icon"></div>
-        <div class="content-item" v-for="item in goodsList">
-            <router-link :to="{path:'/dist/details'}" tag="img" class="content-img" :src="item.thumbnail">立即抢购</router-link>
-            <div class="content-middle">
-                <span><strong>{{item.name}}</strong></span>
-                <!-- <span> <strong>五折优惠</strong></span> -->
-                <div class="price">
-                    <span>{{item.original_price}}</span>
-                    <span>元</span>
-                    <span>{{item.price_spike}}</span>
-                </div>
-                <div class="remain">
-                   <mu-linear-progress :max="200" class="progress" mode="determinate" color="#1e7fea"  :value="item.surplus"/>
-                   <span>剩余{{item.surplus}}份</span>
-                </div>
-            </div>
-             <router-link :to="{path:'/dist/details',query:{id:item.id,lng:116.30387397,lat:39.91481908}}" tag="div" class="content-right">立即抢购</router-link>
-        </div>
+  <!--首页-->
+  <div class="wrapper">
+    <!-- 轮播 -->
+    <app-banner :listImg="listImg"></app-banner>
+    <div class="circle index" @click="routerClickhome">首页</div>
+    <div class="circle personal" @click="routerClickcenter">个人<br>中心</div>
+    <div class="classify">
+      <div class="classify-list list1" v-for="item in classify" @click="clickClassify(item.id)"><img :src="item.icon">{{item.name}}</div>
     </div>
+    <div class="more">
+      <span>大牌抢购</span>
+      <span @click="routerClick">更多
+        <i class="iconfont">&#xe62d;</i>
+      </span>
+    </div>
+    <div class="left-icon"></div>
+    <div class="content-item" v-for="item in goodsList">
+      <router-link :to="{path:'/dist/details'}" tag="img" class="content-img" :src="item.thumbnail">立即抢购</router-link>
+      <div class="content-middle">
+        <span>
+          <strong>{{item.name}}</strong>
+        </span>
+        <!-- <span> <strong>五折优惠</strong></span> -->
+        <div class="price">
+          <span>{{item.original_price}}</span>
+          <span>元</span>
+          <span>{{item.price_spike}}</span>
+        </div>
+        <div class="remain">
+          <mu-linear-progress :max="200" class="progress" mode="determinate" color="#1e7fea" :value="item.surplus" />
+          <span>剩余{{item.surplus}}份</span>
+        </div>
+      </div>
+      <router-link :to="{path:'/dist/details',query:{id:item.id,lng:116.30387397,lat:39.91481908}}" tag="div" class="content-right">立即抢购</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 import $ from "jquery";
 import Banner from "../components/Banner.vue";
-import api from '@/api';
+import api from "@/api";
 import axios from "axios";
 export default {
   components: {
@@ -46,59 +50,48 @@ export default {
     return {
       listImg: [],
       items: [],
-      classify:[],
-      goodsList:[],
-      basePath:"http://dev.mp.duduapp.net",
-      has_id:"1wxAvPWzQro2G4RXkBrd",
-      config:[],
-      fan_id:30,
-      
+      classify: [],
+      goodsList: [],
+      basePath: "http://dev.mp.duduapp.net",
+      has_id: "1wxAvPWzQro2G4RXkBrd",
+      config: [],
+      fan_id: 30
     };
   },
   // 组件创建完后获取数据，
-  created() {
-  
+  created() {},
+  mounted() {
+    this.getImgData();
+    this.getClassifyData();
+    this.getGoodsData();
   },
-   mounted(){
-    this.getImgData()
-    this.getClassifyData()
-    this.getGoodsData()
-   },
   methods: {
     routerClick() {
       this.$router.push("/dist/more");
     },
     routerClickcenter() {
-      this.$router.push({path:"/dist/mycenter",query:{fan_id:this.fan_id}});
-    },clickClassify(classId){
-      this.$router.push({path:"/dist/more",query:{classId:classId}});
+      this.$router.push({
+        path: "/dist/mycenter",
+        query: { fan_id: this.fan_id }
+      });
+    },
+    clickClassify(classId) {
+      this.$router.push({ path: "/dist/more", query: { classId: classId } });
     },
     routerClickhome() {
       this.$router.go(0);
     },
-     // 轮播图数据获取
-    getImgData() {
-      let that = this
-      let url=`${this.basePath}/h5/${this.has_id}?action=carousel_images`
-      axios.get(url,{
-         headers: {'Token': 'elo4aEFQdDVMMGZwMFJVb3pub1Rqd1piSklGclY4ZjBjNSthOXNUd1VORT0.'},
-      }).then(function(response) {
-        that.listImg = response.data.data
-        // console.log(that.listImg)
-      })
+    // 轮播图数据获取
+    async getImgData() {
+      const { data } = await api.get("carousel_images");
+      this.listImg = data.data;
     },
-     // 分类数据获取
-    getClassifyData(){
-      let that = this
-      let url=`${this.basePath}/h5/${this.has_id}?action=classifications`
-      axios.get(url,{
-         headers: {'Token': 'elo4aEFQdDVMMGZwMFJVb3pub1Rqd1piSklGclY4ZjBjNSthOXNUd1VORT0.'},
-      }).then(function(response) {
-      that.classify = response.data.data
-      // console.log(that.classify)
-      })
+    // 分类数据获取
+    async getClassifyData() {
+      const { data } = await api.get("classifications");
+      this.classify = data.data;
     },
-     // 商品数据获取
+    // 商品数据获取
     // getGoodsData(classId){
     //   let that = this
     //   let index = index
@@ -110,18 +103,13 @@ export default {
     //   console.log(that.goodsList)
     //   })
     // }
-    getGoodsData(){
-      let that = this
-      let index = index
-      let url=`${this.basePath}/h5/${this.has_id}?action=goods_list&order[]=${index}`
-      axios.get(url,{
-         headers: {'Token': 'elo4aEFQdDVMMGZwMFJVb3pub1Rqd1piSklGclY4ZjBjNSthOXNUd1VORT0.'},
-      }).then(function(response) {
-      that.goodsList = response.data.data
-      console.log(that.goodsList)
-      })
+    async getGoodsData() {
+      let index = index;
+      const { data } = await api.get("goods_list", {
+        "order[]": index
+      });
+      this.goodsList = data.data;
     }
-    
   }
 };
 </script>
@@ -144,7 +132,7 @@ export default {
     background-color: #232323;
     color: #fff;
     font-size: rem(22);
-     opacity: .8;
+    opacity: 0.8;
     z-index: 1;
   }
   .index {
@@ -195,12 +183,14 @@ export default {
       font-size: rem(30);
       color: #17bafb;
       margin-right: rem(35);
-        .iconfont{
-          font-family:"iconfont"; font-style:normal;margin-left: rem(13);
-          }
+      .iconfont {
+        font-family: "iconfont";
+        font-style: normal;
+        margin-left: rem(13);
+      }
     }
   }
-  .left-icon{
+  .left-icon {
     width: rem(15);
     height: rem(45);
     background-color: #1e7fea;
@@ -225,7 +215,7 @@ export default {
       justify-content: space-between;
       margin: rem(30) 0;
       span:nth-child(1) {
-        width:rem(300);
+        width: rem(300);
         font-size: rem(30);
         line-height: rem(30);
       }
